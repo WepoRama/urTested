c7ac72750e1e0c4bc550008faf12c30fc480d3d7
Elin Gylfadottir <egyl14@gmail.com>
2013-02-10 19:36:41 +0000

    fyrsta gagnagrunns uppastunga, profapartur

--- pythonTestCenter/pythonTestCenter/helloWorld/models.py
+++ pythonTestCenter/pythonTestCenter/helloWorld/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    test_id = models.IntegerField(primary_key=true)
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=20)
+    expires_date = models.DateField()
+
+class Question(models.Model):
+    question_id = models.IntegerField(primary_key=true)
+    test_id = models.IntegerField()
+    question = models.CharField(max_length=50)
+    points = models.IntegerField()
+class Answer (models.Model):
+    answer_id = models.IntegerField(primary_key=true)
+    question_id = models.IntegerField()
+    correct = models.BooleanField()
+    answer = models.CharField(max_length=50)
