cbb947eb904be7c7c536f4e8e84da600050dfaa6
Sigrun Inga Kristinsdottir <sigrunk08@ru.is>
2013-02-11 13:21:58 +0000

    User clsss created

--- pythonTestCenter/pythonTestCenter/helloWorld/models.py
+++ pythonTestCenter/pythonTestCenter/helloWorld/models.py
@@ -17,3 +17,11 @@ class Answer (models.Model):
     question_id = models.IntegerField()
     correct = models.BooleanField()
     answer = models.CharField(max_length=50)
+
+class User (models.User):
+    username = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    is_staff = models.BooleanField()
