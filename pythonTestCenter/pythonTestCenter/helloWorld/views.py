397dcf4ee5d7e7fdcc57384225e6dd66df0c8870
Sigrun Inga Kristinsdottir <sigrunk08@ru.is>
2013-02-14 22:56:42 +0000

    Breyting og viðbætur á createTest view

--- pythonTestCenter/pythonTestCenter/helloWorld/views.py
+++ pythonTestCenter/pythonTestCenter/helloWorld/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 def home(request):
     return HttpResponse(render_to_string(
-                                        '.createTest.html',
+                                        'createTest.html',
                                         #{'content': 'Hello wepoRama'}
                                         ))
