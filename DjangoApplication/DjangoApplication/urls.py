4792fd6b68eb76e02bc4e3ed723f538c35a4187b
Elin Gylfadottir <egyl14@gmail.com>
2013-02-14 19:01:41 +0000

    Hello World, in da house

--- DjangoApplication/DjangoApplication/urls.py
+++ DjangoApplication/DjangoApplication/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'DjangoApplication.MyFirstApp.views.home', name='home'),
     # Examples:
     # url(r'^$', 'DjangoApplication.views.home', name='home'),
     # url(r'^DjangoApplication/', include('DjangoApplication.DjangoApplication.urls')),
