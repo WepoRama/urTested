ca6bd6e8c9415c442a43c46083c86bb848555bb4
Carsten Petersen <cpetersen75@gmail.com>
2013-02-14 10:47:29 +0000

    Nýtt tilraun i að commit-a

--- pythonTestCenter/pythonTestCenter/helloWorld/models.py
+++ pythonTestCenter/pythonTestCenter/helloWorld/models.py
@@ -1,31 +1,31 @@
-#from django.db import models
+from django.db import models
 
-## Create your models here.
-#class Test(models.Model):
-#    test_id = models.IntegerField(primary_key=true)
-#    name = models.CharField(max_length=50)
-#    author = models.CharField(max_length=20)
-#    expires_date = models.DateField()
+# Create your models here.
+class Test(models.Model):
+    test_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=20)
+    expires_date = models.DateField()
 
-#class Question(models.Model):
-#    question_id = models.IntegerField(primary_key=true)
-#    test_id = models.IntegerField()
-#    question = models.CharField(max_length=50)
-#    points = models.IntegerField()
+class Question(models.Model):
+    question_id = models.IntegerField()
+    test_id = models.IntegerField()
+    question = models.CharField(max_length=50)
+    points = models.IntegerField()
 
-#class Answer (models.Model):
-#    answer_id = models.IntegerField(primary_key=true)
-#    question_id = models.IntegerField()
-#    correct = models.BooleanField()
-#    answer = models.CharField(max_length=50)
+class Answer (models.Model):
+    answer_id = models.IntegerField()
+    question_id = models.IntegerField()
+    correct = models.BooleanField()
+    answer = models.CharField(max_length=50)
 
-## class for user and userrole
-#class User (models.Model):
-#    username = models.CharField(max_length=30)
-#    first_name = models.CharField(max_length=30)
-#    last_name = models.CharField(max_length=30)
-#    email = models.CharField(max_length=30)
-#    password = models.CharField(max_length=30)
-#    is_staff = models.BooleanField()
-#    is_superuser = models.BooleanField()
-#    last_login = models.DateTimeField()
+# class for user and userrole
+class User (models.Model):
+    username = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    is_staff = models.BooleanField()
+    is_superuser = models.BooleanField()
+    last_login = models.DateTimeField()
--- pythonTestCenter/pythonTestCenter/settings.py
+++ pythonTestCenter/pythonTestCenter/settings.py
@@ -15,8 +15,8 @@ PROJECT_DIR = os.path.dirname(__file__)
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_DIR, 'ExamsDB.db'), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
