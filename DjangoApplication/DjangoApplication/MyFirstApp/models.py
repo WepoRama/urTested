9d09eaf270493311edfdb0f74c07946db5e47998
Elin Gylfadottir <egyl14@gmail.com>
2013-02-14 21:41:05 +0000

    DB attempt

--- DjangoApplication/DjangoApplication/MyFirstApp/models.py
+++ DjangoApplication/DjangoApplication/MyFirstApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    test_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=20)
+    expires_date = models.DateField()
+
+class Question(models.Model):
+    question_id = models.IntegerField(primary_key=True)
+    test_id = models.IntegerField()
+    question = models.CharField(max_length=50)
+    points = models.IntegerField()
+
+class Answer (models.Model):
+    answer_id = models.IntegerField(primary_key=True)
+    question_id = models.IntegerField()
+    correct = models.BooleanField()
+    answer = models.CharField(max_length=50)
--- DjangoApplication/DjangoApplication/MyFirstApp/views.py
+++ DjangoApplication/DjangoApplication/MyFirstApp/views.py
@@ -1,8 +1,21 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+def getTest(intId):
+    #Connect to the MySQL sample database 'world'
+    cur = django.db.connections['default'].cursor()
+    #Execute a trivial SQL query which returns the name of 
+    #all countries contained in 'world'
+    cur.execute("SELECT name from test")
+    tmp = cur.fetchall()
+    #Clean-up after ourselves
+    cur.close()
+    if intId >= len(tmp):
+        return "countries exhausted"
+    return tmp[intId][0]
+
 def home(request):
     return HttpResponse(render_to_string(
                                         'index.html',
-                                        {'content': 'Hello World'}
+                                        {'content': getTest(1)}
                                         ))
