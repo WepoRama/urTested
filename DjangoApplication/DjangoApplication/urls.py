fe40f9fe1e13d2ad6d0ccf6ce481f5a78a570de5
Elin Gylfadottir <egyl14@gmail.com>
2013-02-17 21:51:37 +0000

    betri svor

--- DjangoApplication/DjangoApplication/urls.py
+++ DjangoApplication/DjangoApplication/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     #url(r'^$', 'DjangoApplication.MyFirstApp.views.home', name='home'),
-    url(r'^$', 'DjangoApplication.MyFirstApp.views.createTest', name='createTest'),
+    url(r'^createTest$', 'DjangoApplication.MyFirstApp.views.createTest', name='createTest'),
     url(r'^createIt/$', 'DjangoApplication.MyFirstApp.views.createIt', name='createIt'),
     #url(r'^addQuestion//$', 'DjangoApplication.MyFirstApp.views.addQuestion', name='addQuestion'),
     url(r'^(?P<test_id>\d+)/addQuestion/$', 'DjangoApplication.MyFirstApp.views.addQuestion'),
