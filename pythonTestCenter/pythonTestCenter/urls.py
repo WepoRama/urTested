3cabae44fde3fb7226bf364d36140e0a82b98eca
Carsten Petersen <cpetersen75@gmail.com>
2013-02-17 17:37:12 +0000

    Færa login yfir á forsiðunni

--- pythonTestCenter/pythonTestCenter/urls.py
+++ pythonTestCenter/pythonTestCenter/urls.py
@@ -2,14 +2,15 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-#admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'pythonTestCenter.helloWorld.views.home', name='home'),
+    #url(r'^$', 'pythonTestCenter.helloWorld.views.home', name='home'),
     # url(r'^$', 'pythonTestCenter.views.home', name='home'),
     # url(r'^pythonTestCenter/', include('pythonTestCenter.pythonTestCenter.urls')),
     url(r'accounts/login', 'django.contrib.auth.views.login', name='login'),
+    url(r'^$', 'django.contrib.auth.views.login', name='login'),
     url(r'accounts/logout', 'django.contrib.auth.views.logout', name='logout'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
