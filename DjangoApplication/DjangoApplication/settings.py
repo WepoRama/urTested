a6cab7cec300d47d510ca1510ac3ca13d9350709
TheBandStand <egyl14@gmail.com>
2013-02-18 21:59:50 +0000

    d: allsstadar

--- DjangoApplication/DjangoApplication/settings.py
+++ DjangoApplication/DjangoApplication/settings.py
@@ -32,7 +32,8 @@ DATABASES = {
     },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\Ella\\Documents\\_HR\\winaz\\urTested\\DjangoApplication\\urtested2.db',                      # Or path to database file if using sqlite3.
+        #'NAME': 'C:\\Users\\Ella\\Documents\\_HR\\winaz\\urTested\\DjangoApplication\\urtested2.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'D:\\home\\site\\wwwroot\\DjangoApplication',
     },
     #'azure': {
     #    'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
